52d98650c5961a86a3a0228237da72e474891aea
Michael Orr <michael@cloudspace.com>
2011-07-01 12:48:23 -0400

    initial generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevisableTest::Application.config.secret_token = 'a2f0aa267c96807e06c5fc23254e88ab23f41af59c3927f3ab6246a2a9b0f23e8a56d7c5f5009ed61474bbe9f72f27bbb91ce7f08d97150ec2c4ebcd597513e1'
